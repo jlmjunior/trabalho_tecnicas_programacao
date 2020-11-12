d1922d6e61918e9619fcd08e1d1b6680f5d89d71
José L. Martinez <57760208+jlmjunior@users.noreply.github.com>
2020-11-12 16:55:31 -0300

    Add files via upload

---
+++ Farmacia/main.cpp
@@ -0,0 +1,573 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <time.h>
+#include "estrutura.h"
+#include "util.h"
+
+void CadastrarCliente(cliente* *listaClientes);
+void CadastrarProduto(produto* *listaProdutos);
+void ImprimeClientes(cliente *listaClientes);
+void ImprimeProdutos(produto *listaProdutos);
+void CarregarProdutos(produto* *listaProdutos);
+void VenderProduto(produto* *listaProdutos, cliente *listaClientes);
+void CadastrarServico(servico* *listaServicos);
+void ExecutarServico(servico *listaServicos);
+void VerFaturamento();
+void ImprimeProdutosPorValor(produto *listaProdutos);
+
+int main()
+{ 
+    cliente *listaClientes = NULL;
+    produto *listaProdutos = NULL;
+    servico *listaServicos = NULL;
+
+    int menu;
+
+    CarregarProdutos(&listaProdutos);
+
+    while (true)
+    {
+        puts("# MENU PRINCIPAL\n"); 
+        puts("[1] Cadastrar Cliente");
+        puts("[2] Imprimir Clientes");
+        puts("[3] Cadastrar produto");
+        puts("[4] Imprimir Produtos");
+        puts("[5] Efetuar Venda");
+        puts("[6] Cadastrar Servico");
+        puts("[7] Executar Servico");
+        puts("[8] Ver Faturamento");
+        puts("[9] Imprimir Produtos de maior valor");
+        puts("[0] Sair");
+
+        printf("\n[-]: ");
+        fflush(stdin);
+        scanf("%d", &menu);
+
+        system("cls");
+
+        switch (menu)
+        {
+            case 0:
+                exit(true);
+
+            case 1:
+                CadastrarCliente(&listaClientes);
+                break;
+            
+            case 2:
+                ImprimeClientes(listaClientes);
+                break;
+            case 3:
+                CadastrarProduto(&listaProdutos);
+                GravarEstoque(listaProdutos);
+                break;  
+            case 4:
+                ImprimeProdutos(listaProdutos);
+                break;
+            case 5:
+                VenderProduto(&listaProdutos, listaClientes);
+                break;
+            case 6:
+                CadastrarServico(&listaServicos);
+                break;
+            case 7:
+                ExecutarServico(listaServicos);
+                break;
+            case 8:
+                VerFaturamento();
+                break;
+            case 9:
+                ImprimeProdutosPorValor(listaProdutos);
+                break;
+                
+            default:
+                puts("[Opcao invalida]\n");
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void CadastrarCliente (cliente* *listaClientes)
+{   
+    cliente *no = (cliente*) malloc (sizeof(cliente));
+
+    if (no != NULL)
+    {        
+        puts("# CADASTRO DE CLIENTE\n");
+
+        printf("CPF: ");
+        fflush(stdin);
+        gets(no->pessoa.cpf);
+
+        if (ExisteCliente(*listaClientes, no->pessoa.cpf))
+        {
+            free(no);
+            puts("\n[CPF INVALIDO! Cliente ja cadastrado]\n");
+            
+            system("pause");
+            system("cls");
+
+            return;
+        }
+
+        printf("NOME: ");
+        fflush(stdin);
+        gets(no->pessoa.nome);
+
+        printf("ENDERECO: ");
+        fflush(stdin);
+        gets(no->pessoa.endereco);
+
+        no->prox = NULL;
+
+        if (*listaClientes == NULL) 
+        {
+            *listaClientes = no;
+        }
+        else
+        {
+            cliente *aux = *listaClientes;
+
+            while (aux->prox != NULL)
+                aux = aux->prox;
+            
+            aux->prox = no;
+        }
+    } 
+    else
+    {
+        puts("Erro ao alocar memoria");
+    }
+
+    puts("\n[CLIENTE CADASTRADO COM SUCESSO]\n");
+    system("pause");
+    system("cls");
+}
+
+void ImprimeClientes(cliente *listaClientes)
+{
+    int countLista = 0;
+
+    if (listaClientes == NULL)
+    {
+        puts("[NENHUM CLIENTE CADASTRADO]\n");
+    }
+    else
+    {
+        while (listaClientes != NULL)
+        {
+            puts("----------------------------------------------------");
+            printf("Cliente: %d", countLista + 1);
+            printf("\nNome: %s", listaClientes->pessoa.nome);
+            printf("\nCPF: %s\n", listaClientes->pessoa.cpf);
+            puts("");
+
+            listaClientes = listaClientes->prox;
+            countLista++;
+        }
+
+        puts("----------------------------------------------------");
+        printf("[?] Quantidade de clientes: %d\n\n", countLista);
+    }
+
+    system("pause");
+    system("cls");
+}
+
+void CadastrarProduto(produto* *listaProdutos) 
+{   
+    produto *no = (produto*) malloc (sizeof(produto));
+
+    puts("# CADASTRO DE PRODUTO\n");
+
+    printf("CODIGO DO PRODUTO: ");
+    scanf("%d", &no->info.codigo);
+
+    bool existeProduto = BuscarProduto(*listaProdutos, no->info.codigo).codigo != -1 ? true : false;
+
+    if (existeProduto || no->info.codigo < 0)
+    {
+        free(no);
+        puts("\n[CODIGO INVALIDO! Produto ja cadastrado ou codigo negativo]\n");
+        
+        system("pause");
+        system("cls");
+
+        return;
+    }
+
+    printf("NOME DO PRODUTO: ");
+    fflush(stdin);
+    gets(no->info.nome);
+    
+    printf("FABRICANTE: ");
+    fflush(stdin);
+    gets(no->info.fabricante);
+
+    printf("DATA DE VALIDADE: ");
+    fflush(stdin);
+    gets(no->info.data_validade);
+    
+    printf("DATA DE FABRICACAO ");
+    fflush(stdin);
+    gets(no->info.data_fabricacao);
+
+    printf("PRECO: ");
+    scanf("%f", &no->info.preco);
+
+    no->proximo = NULL;
+
+    if (*listaProdutos == NULL)
+    {
+        *listaProdutos = no;
+    }
+    else
+    {
+        produto *aux = *listaProdutos;
+        
+        while (aux->proximo != NULL)
+            aux = aux->proximo;
+            
+        aux->proximo = no;
+    }
+
+    puts("\n[PRODUTO CADASTRADO COM SUCESSO]\n");
+
+    system("pause");
+    system("cls");
+}
+
+void CarregarProdutos(produto* *listaProdutos)
+{
+    FILE *arquivo = fopen("estoque.txt", "r");
+    char *leuArquivo;
+    char linha[200] = "\0";
+
+    if (arquivo == NULL)
+    {
+        puts("[ERRO AO CARREGAR ARQUIVO DE ESTOQUE]\n");
+        return;
+    }
+
+    while (!feof(arquivo))
+    {
+        leuArquivo = fgets(linha, 200, arquivo);
+        
+        if (!leuArquivo || strlen(linha) == 0) break; 
+
+        produto *no = (produto*) malloc (sizeof(produto));
+
+        if (no == NULL)
+        {
+            puts("\nErro ao alocar memoria");
+            return;
+        }
+
+        no->info = ConveterTextoParaProduto(linha);
+        no->proximo = NULL;
+
+        if (*listaProdutos == NULL) 
+        {
+            *listaProdutos = no;
+        }
+        else
+        {
+            produto *aux = *listaProdutos;
+
+            while (aux->proximo != NULL) 
+                aux = aux->proximo;
+            
+            aux->proximo = no;
+        }
+    }
+
+    fclose(arquivo);
+}
+
+void ImprimeProdutos(produto *listaProdutos)
+{
+    int count = 0;
+
+    if (listaProdutos == NULL)
+    {
+        puts("[LISTA VAZIA]\n");
+    }
+    else
+    {
+        while (listaProdutos != NULL)
+        {
+            puts("----------------------------------------------------");
+            printf("Codigo: %d", listaProdutos->info.codigo);
+            printf("\nNome: %s", listaProdutos->info.nome);
+            printf("\nPreco: %.2f", listaProdutos->info.preco);
+            printf("\nData fabricacao: %s", listaProdutos->info.data_fabricacao);
+            printf("\nData validade: %s", listaProdutos->info.data_validade);
+            printf("\nFabricante: %s\n", listaProdutos->info.fabricante);
+            puts("");
+
+            listaProdutos = listaProdutos->proximo;
+            count++;
+        }
+
+        puts("----------------------------------------------------");
+        printf("[?] Quantidade de produtos: %d\n\n", count);
+    }
+
+    system("pause");
+    system("cls");
+}
+
+void VenderProduto(produto* *listaProdutos, cliente *listaClientes)
+{
+    if (*listaProdutos == NULL)
+    {
+        puts("[NENHUM PRODUTO CADASTRADO]\n");
+    }
+    else
+    {
+        char cpf[20] = "\0";
+
+        printf("[-] CPF DO CLIENTE: ");
+        fflush(stdin);
+        gets(cpf);
+
+        system("cls");
+        
+        if (ExisteCliente(listaClientes, cpf))
+        {
+            bool continuarMenu = true;
+            carrinho *listaCarrinho = NULL;
+
+            while (continuarMenu)
+            {
+                int carrinho;
+
+                puts("# MENU DE VENDA\n");
+                puts("[1] Adicionar ao carrinho");
+                puts("[2] Finalizar compra");
+                puts("[9] Cancelar");
+                
+                printf("\n[-]: ");
+                scanf("%d", &carrinho);
+
+                system("cls");
+
+                switch (carrinho)
+                {
+                    case 1:
+                        AdicionarAoCarrinho(&listaCarrinho, *listaProdutos, cpf);
+                        break;
+                    case 2:
+                        FinalizarCompra(&listaCarrinho, listaProdutos);
+                        break;
+                    case 9:
+                        continuarMenu = false;
+                        break;
+
+                    default:
+                        puts("[OPCAO INVALIDA]\n");
+                        break;
+                }
+            }
+
+            LimparCarrinho(&listaCarrinho);
+        }
+        else
+        {
+            puts("[CLIENTE NAO ENCONTRADO]\n");
+        }
+    }
+    
+    system("pause");
+    system("cls");
+}
+
+void CadastrarServico(servico* *listaServicos)
+{
+    servico *no = (servico*) malloc (sizeof(servico));
+
+    if (no != NULL)
+    {
+        puts("# CADASTRO DE SERVICO\n");
+
+        printf("CODIGO DO SERVICO: ");
+        scanf("%d", &no->info.codigo);
+
+        if (ExisteServico(*listaServicos, no->info.codigo))
+        {
+            free(no);
+            puts("\n[CODIGO INVALIDO! Servico ja cadastrado]\n");
+            
+            system("pause");
+            system("cls");
+
+            return;
+        }
+        
+        printf("NOME DO SERVICO: ");
+        fflush(stdin);
+        gets(no->info.nome);
+
+        printf("PRECO: ");
+        scanf("%f", &no->info.preco);
+
+        no->prox = NULL;
+
+        if (*listaServicos == NULL) 
+        {
+            *listaServicos = no;
+        }
+        else
+        {
+            servico *aux = *listaServicos;
+
+            while (aux->prox != NULL)
+                aux = aux->prox;
+            
+            aux->prox = no;
+        }
+
+        puts("\n[SERVICO CADASTRADO COM SUCESSO]\n");
+    } 
+    else
+    {
+        puts("\n[ERRO AO ALOCAR MEMORIA]\n");
+    }
+
+    system("pause");
+    system("cls");
+}
+
+void ExecutarServico(servico *listaServicos)
+{
+    if (listaServicos == NULL)
+    {
+        puts("[NENHUM SERVICO CADASTRADO]\n");
+    }
+    else
+    {
+        servico *auxLista = listaServicos;
+        
+        puts("# EXECUCAO DE SERVICO\n");
+
+        while (auxLista != NULL)
+        {
+            puts("----------------------------------------------------");
+            printf("Codigo: %d", auxLista->info.codigo);
+            printf("\nNome: %s", auxLista->info.nome);
+            printf("\nPreco: %.2f\n", auxLista->info.preco);
+            puts("");
+
+            auxLista = auxLista->prox;
+        }
+
+        puts("----------------------------------------------------");
+
+        int codigoServico;
+
+        printf("[-]: ");
+        scanf("%d", &codigoServico);
+
+        if (ExisteServico(listaServicos, codigoServico))
+        {
+            infoServico serv = BuscarServico(listaServicos, codigoServico);
+
+            ContabilizarFaturamento(serv.preco);
+
+            puts("\n[SERVICO EXECUTADO COM SUCESSO]\n");
+        }
+        else
+        {
+            puts("\n[SERVICO NAO ENCONTRADO]\n");
+        }
+        
+    }
+
+    system("pause");
+    system("cls");    
+}
+
+void VerFaturamento()
+{
+    FILE *arquivo = fopen("faturamento.txt", "r");
+    char *leuArquivo;
+    char linha[20] = "\0";
+
+    puts("# FATURAMENTO\n");
+
+    if (arquivo == NULL)
+    {
+        puts("\n[ERRO AO LER ARQUIVO DE FATURAMENTO]\n");
+    }
+    else
+    {
+        leuArquivo = fgets(linha, 20, arquivo);
+
+        printf("TOTAL FATURADO: R$ %s\n\n", (strlen(linha) > 0 ? linha : "0.00"));
+    }
+
+    fclose(arquivo);
+
+    system("pause");
+    system("cls");
+}
+
+void ImprimeProdutosPorValor(produto *listaProdutos)
+{
+    int maxRank = 5;
+
+    if (listaProdutos == NULL)
+    {
+        puts("[LISTA VAZIA]");
+    }
+    else
+    {
+        puts("# TOP 5 VALOR\n");
+
+        int tamanhoLista = TamanhoListaProdutos(listaProdutos);
+        int tamanhoRank = (tamanhoLista > maxRank ? maxRank : tamanhoLista);
+
+        int ignorados[tamanhoRank], countIgnorados = 0;
+
+        float tabela[tamanhoRank][2];
+
+        for (int i = 0; i < tamanhoRank; i++)
+        {
+            produto *aux = listaProdutos;
+            float atual[2] = {0,0};
+
+            while (aux != NULL)
+            {
+                if (aux->info.preco > atual[1] && !EhIgnorado(ignorados, countIgnorados, aux->info.codigo))
+                {
+                    atual[0] = (float)aux->info.codigo;
+                    atual[1] = aux->info.preco;
+                }
+
+                aux = aux->proximo;
+            }
+
+            tabela[i][0] = atual[0];
+            tabela[i][1] = atual[1];
+
+            ignorados[i] = (int)tabela[i][0];
+            countIgnorados++;
+        }
+
+        for (int i = 0; i < tamanhoRank; i++)
+        {
+            puts("------------------------------------------");
+            printf("(%d) PRODUTO: %.0f // VALOR: %.2f\n", i+1, tabela[i][0], tabela[i][1]);
+        }
+    }
+    
+    puts("");
+    system("pause");
+    system("cls");
+}
---
+++ Farmacia/util.h
@@ -0,0 +1,420 @@
+
+infoProduto ConveterTextoParaProduto(char texto[200])
+{
+    infoProduto prod;
+
+    char auxCodigo[10] = "\0";
+    char auxPreco[10] = "\0";
+
+    int y = 0;
+
+    for (int i = 0; i < strlen(texto); i++)
+    {
+        if (texto[i] == ',')
+        {
+            y++;
+            continue;
+        }
+
+        switch (y)
+        {
+            case 0:
+                auxCodigo[strlen(auxCodigo)] = texto[i];
+                break;
+            case 1:
+                prod.data_fabricacao[strlen(prod.data_fabricacao)] = texto[i];
+                break;
+            case 2:
+                prod.data_validade[strlen(prod.data_validade)] = texto[i];
+                break;
+            case 3:
+                prod.fabricante[strlen(prod.fabricante)] = texto[i];
+                break;    
+            case 4:
+                prod.nome[strlen(prod.nome)] = texto[i];
+                break;
+
+            default:
+                auxPreco[strlen(auxPreco)] = texto[i];
+                break;
+        }
+    }
+
+    prod.codigo = atoi(auxCodigo);
+    prod.preco = atof(auxPreco);
+
+    return prod;
+}
+
+infoProduto BuscarProduto(produto *listaProdutos, int codigo)
+{
+    infoProduto prod;
+
+    prod.codigo = -1;
+
+    if (listaProdutos == NULL)
+    {
+        return prod;
+    }
+
+    while (listaProdutos != NULL)
+    {
+        if (listaProdutos->info.codigo == codigo) return listaProdutos->info;
+
+        listaProdutos = listaProdutos->proximo;
+    }
+
+    return prod;
+}
+
+infoServico BuscarServico(servico *listaServicos, int codigo)
+{
+    infoServico serv;
+
+    serv.codigo = -1;
+
+    if (listaServicos == NULL)
+    {
+        return serv;
+    }
+
+    while (listaServicos != NULL)
+    {
+        if (listaServicos->info.codigo == codigo) return listaServicos->info;
+
+        listaServicos = listaServicos->prox;
+    }
+
+    return serv;
+}
+
+void ContabilizarFaturamento(float valor)
+{
+    FILE *arquivo = fopen("faturamento.txt", "r");
+    char *leuArquivo;
+    char linha[50] = "\0";
+
+    if (arquivo == NULL)
+    {
+        puts("\n[ERRO AO LER ARQUIVO DE FATURAMENTO]\n");
+        return;
+    }
+
+    char novoValor[50] = "\0";
+
+    leuArquivo = fgets(linha, 200, arquivo);
+
+    fclose(arquivo);
+
+    arquivo = fopen("faturamento.txt", "w");
+    
+    if (leuArquivo || strlen(linha) > 0)
+    {
+        sprintf(novoValor, "%.2f", valor + atof(linha));
+        fputs(novoValor, arquivo);
+    }
+    else
+    {
+        sprintf(novoValor, "%.2f", valor);
+        fputs(novoValor, arquivo);
+    }
+    
+    fclose(arquivo);
+}
+
+void GravarEstoque(produto *listaProdutos)
+{
+    if (listaProdutos == NULL)
+    {
+        FILE *arquivo = fopen("estoque.txt", "w");
+        fclose(arquivo);
+    }
+    else
+    {
+        FILE *arquivo = fopen("estoque.txt", "w");
+
+        if (arquivo == NULL)
+        {
+            puts("\n[FALHA AO CRIAR ARQUIVO DE ESTOQUE]\n");
+            return;
+        }
+
+        while (listaProdutos != NULL)
+        {
+            char produtoFormatado[200];
+            int resultado = 0;
+
+            sprintf(produtoFormatado, "%d,%s,%s,%s,%s,%.2f%s"
+                    ,listaProdutos->info.codigo
+                    ,listaProdutos->info.data_fabricacao
+                    ,listaProdutos->info.data_validade
+                    ,listaProdutos->info.fabricante
+                    ,listaProdutos->info.nome
+                    ,listaProdutos->info.preco
+                    ,(listaProdutos->proximo == NULL ? "" : "\n"));
+
+            resultado = fputs(produtoFormatado, arquivo);
+
+            if (resultado == EOF) puts("\n[FALHA AO GRAVAR NO ARQUIVO DE ESTOQUE]\n");
+            
+            listaProdutos = listaProdutos->proximo;
+        }
+
+        fclose(arquivo);
+    }
+}
+
+bool ExisteCliente(cliente *listaClientes, char cpf[20])
+{
+    if (listaClientes == NULL)
+    {
+        return false;
+    }
+
+    while (listaClientes != NULL)
+    {
+        if (!strcmp(listaClientes->pessoa.cpf, cpf)) return true;
+
+        listaClientes = listaClientes->prox;
+    }
+
+    return false;
+}
+
+bool ExisteServico(servico *listaServicos, int codigo)
+{
+    if (listaServicos == NULL)
+    {
+        return false;
+    }
+
+    while (listaServicos != NULL)
+    {
+        if (listaServicos->info.codigo == codigo) return true;
+
+        listaServicos = listaServicos->prox;
+    }
+
+    return false;
+}
+
+bool ContemNoCarrinho(carrinho *listaCarrinho, int codigo)
+{
+    if (listaCarrinho == NULL)
+    {
+        return false;
+    }
+
+    while (listaCarrinho != NULL)
+    {
+        if (listaCarrinho->info.codigoProduto == codigo) return true;
+
+        listaCarrinho = listaCarrinho->prox;
+    }
+
+    return false;
+}
+
+void EmitirComprovante(carrinho *listaCarrinho)
+{
+    char diretorio[40] = "\0";
+    sprintf(diretorio, "./Comprovantes/comprovante_%ld.txt", time(NULL));
+
+    FILE *arquivo = fopen(diretorio, "w");
+    
+    if (arquivo == NULL)
+    {
+        puts("Falha ao carregar arquivo de estoque");
+        return;
+    }
+
+    time_t dataAtual = time(0);
+    time(&dataAtual);
+
+    float valorTotal = 0;
+    char cabecalho[200] = "\0";
+
+    sprintf(cabecalho, "#COMPROVANTE#\n\n%s\nCPF Cliente: %s\n", ctime(&dataAtual), listaCarrinho->info.cpf);
+
+    fputs(cabecalho, arquivo);
+
+    while (listaCarrinho != NULL)
+    {
+        char corpo[400] = "\0";
+
+        sprintf(corpo, "\nCodigo: %d\nNome do produto: %s\nTarifa: R$ %.2f\n"
+                ,listaCarrinho->info.codigoProduto
+                ,listaCarrinho->info.nomeProduto
+                ,listaCarrinho->info.precoProduto);
+        strcat(corpo, "--------------------------------------------------------------");
+
+        fputs(corpo, arquivo);
+        
+        valorTotal += listaCarrinho->info.precoProduto;
+
+        listaCarrinho = listaCarrinho->prox;
+    }
+
+    char rodape[100] = "\0";
+
+    sprintf(rodape, "\n\nTarifa total: R$ %.2f", valorTotal);
+
+    fputs(rodape, arquivo);
+
+    fclose(arquivo);
+
+    puts("\n[COMPROVANTE GERADO]\n");
+
+    ContabilizarFaturamento(valorTotal);
+}
+
+void AdicionarAoCarrinho(carrinho* *listaCarrinho, produto *listaProdutos , char cpf[20])
+{
+    int codigo;
+
+    printf("[-] CODIGO DO PRODUTO: ");
+    scanf("%d", &codigo);
+
+    if (ContemNoCarrinho(*listaCarrinho, codigo))
+    {
+        puts("\n[PRODUTO JA ESTA NO CARRINHO]\n");
+
+        system("pause");
+        system("cls");
+
+        return;
+    }
+
+    infoProduto prod = BuscarProduto(listaProdutos, codigo);
+
+    if (prod.codigo != -1)
+    {
+        carrinho *no = (carrinho*) malloc (sizeof(carrinho));
+
+        no->prox = NULL;
+        no->info.codigoProduto = prod.codigo;
+        no->info.precoProduto = prod.preco;
+        strcpy(no->info.nomeProduto, prod.nome);
+        strcpy(no->info.cpf, cpf);
+
+        if (*listaCarrinho == NULL)
+        {
+            *listaCarrinho = no;
+        }
+        else
+        {
+            carrinho *aux = *listaCarrinho;
+            
+            while (aux->prox != NULL)
+                aux = aux->prox;
+                
+            aux->prox = no;
+        }
+
+        puts("\n[PRODUTO ADICIONADO AO CARRINHO]\n");
+    }
+    else
+    {
+        puts("\n[PRODUTO NAO ENCONTRADO]\n");
+    }
+
+    system("pause");
+    system("cls");
+}
+
+void ExcluirProduto(produto* *listaProdutos, int codigo)
+{     
+    produto *aux1 = *listaProdutos;
+    produto *aux2 = aux1->proximo;
+    
+    if (aux1->info.codigo == codigo)
+    {
+        *listaProdutos = aux2;
+        free(aux1);
+
+        return;
+    }
+
+    while (aux2 != NULL)
+    {
+        if (aux2->info.codigo == codigo)
+        {
+            aux1->proximo = aux2->proximo;
+            free(aux2);
+
+            return;
+        }
+
+        aux1 = aux2;
+        aux2 = aux2->proximo;		
+    }
+}
+
+void LimparCarrinho(carrinho* *listaCarrinho)
+{ 
+    if (*listaCarrinho != NULL)
+    {
+	    carrinho *aux = *listaCarrinho;
+
+        while( aux != NULL )
+        {     
+            *listaCarrinho = aux->prox;     
+            free(aux);      
+            aux = *listaCarrinho;               
+		 }
+	}      
+}
+
+void FinalizarCompra(carrinho* *listaCarrinho, produto* *listaProdutos)
+{
+    carrinho *auxCarrinho = *listaCarrinho;
+
+    if (*listaCarrinho == NULL)
+    {
+        puts("[CARRINHO VAZIO]\n");
+    }
+    else
+    {
+        EmitirComprovante(auxCarrinho);
+
+        while (auxCarrinho != NULL)
+        {
+            ExcluirProduto(listaProdutos, auxCarrinho->info.codigoProduto);
+
+            auxCarrinho = auxCarrinho->prox;
+        }
+
+        GravarEstoque(*listaProdutos);
+        LimparCarrinho(listaCarrinho);
+
+        puts("[COMPRA FINALIZADA COM SUCESSO]\n"); 
+    }
+
+    system("pause");
+    system("cls");
+}
+
+int TamanhoListaProdutos(produto *listaProdutos)
+{
+    if (listaProdutos == NULL) return 0;
+
+    int count = 0;
+
+    while (listaProdutos != NULL)
+    {
+        listaProdutos = listaProdutos->proximo;
+        count++;
+    }
+
+    return count;
+}
+
+bool EhIgnorado(int lista[], int tamanho, int codigo)
+{
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (lista[i] == codigo) return true;
+    }
+
+    return false;
+}
